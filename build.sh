4ec24c1f214a1cf5bb3656e19293f721fb7715f4
Marcus <marcus.almendro@gmail.com>
2019-12-21 18:21:16 -0300

    build linux

---
+++ build.sh
@@ -0,0 +1,25 @@
+cd services
+sbt publishLocal
+cd ..
+
+mkdir account/lib
+mkdir account-sync/lib
+mkdir transfer/lib
+
+cp services/target/scala-2.12/services_2.12-0.1.jar account/lib
+cp services/target/scala-2.12/services_2.12-0.1.jar account-sync/lib
+cp services/target/scala-2.12/services_2.12-0.1.jar transfer/lib
+
+cd account
+sbt docker:publishLocal
+cd ..
+
+cd account-sync
+sbt docker:publishLocal
+cd ..
+
+cd transfer
+sbt docker:publishLocal
+cd ..
+
+docker build ./api -t ledger-api:0.1
